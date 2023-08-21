e1e2151ae6c7501c23d6d4223e2821b7d7c96e14
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2023-08-21 21:31:51 +0500

    Add error handling code

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,17 +1,37 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../Components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+    
+    const searchApi = async () => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    term: searchTerm,
+                    location: 'san jose',
+                    limit: 50
+                }
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Something went wrong');
+        }
+    };
 
     return (
         <View>
             <SearchBar
                 searchTerm={searchTerm}
                 onSearchTermChange={(newSearchTerm) => {setSearchTerm(newSearchTerm)}}
-                onTermSubmit={() => {console.log('submitted')}}
-                />
+                onTermSubmit={() => searchApi()}
+            />
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found {results.length} results</Text>
         </View>
     )
 };
